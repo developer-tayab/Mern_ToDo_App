e70e375dac3cdd026ce992f525b176f5472d433c
Muhammad Tayyab <tayyabdeveloper15@gmail.com>
2025-01-16 23:07:43 +0500

    setup Server

---
+++ backend/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extends: true }));
+
+
+
+app.use("/", (req, res) => {
+  res.send("This is working!")
+});
+
+
+
+app.listen(process.env.PORT, (req, res) => {
+  console.log(`Server is running at http://localhost:${process.env.PORT}`);
+});
+
+
